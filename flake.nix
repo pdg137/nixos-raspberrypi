4ad929b329775a2ce341dd1c3da6070d89277e19
Paul Grayson <pdg@alum.mit.edu>
2025-06-01 10:35:41 -0700

    nixpkgs update to disable gst-plugins-bad

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     # use fork to allow disabling modules introduced by mkRemovedOptionModule
     # and similar functions
     # see PR nixos:nixpkgs#398456 (https://github.com/NixOS/nixpkgs/pull/398456)
-    nixpkgs.url = "github:nvmd/nixpkgs/modules-with-keys-unstable";
+    nixpkgs.url = "github:pdg137/nixpkgs/modules-with-keys-unstable-disable-gst-plugins-bad";
     # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     argononed = {
